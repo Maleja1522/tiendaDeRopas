e6a74f3461eb59e9cbc4375984edcd8bb09357e7
Maleja1522 <90017653+Maleja1522@users.noreply.github.com>
2021-09-02 23:26:33 -0500

    Add files via upload

---
+++ tiendaRopaPrueba.js
@@ -0,0 +1,5 @@
+describe("prueba seleccionado woman",function(){
+beforeEach(function(){})
+    cy.visit("http://automationpractice.com/index.php")
+    cy.get('[class=sf-with-ul]').first().click()
+})
